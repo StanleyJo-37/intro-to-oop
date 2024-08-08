d39caf3e4ad2e94fb257f7dd023a7ffebb4ceaad
StanleyJo-37 <stanleyjo13w@gmail.com>
2024-08-08 17:47:47 +0700

    Created main function with test

---
+++ Academic App/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student studentA = new Student("Stanley", 19, 4.00f);
+		studentA.info();
+		
+		Float a = 5.6f;
+		System.out.println(a);
+	}
+
+}
