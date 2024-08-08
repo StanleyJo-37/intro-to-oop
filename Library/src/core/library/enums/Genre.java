4c426e0b32706218a572d1aa3466049b60cd0dbe
StanleyJo-37 <stanleyjo13w@gmail.com>
2024-08-08 17:50:22 +0700

    Created Genre and Topic enum for Book and News

---
+++ Library/src/core/library/enums/Genre.java
@@ -0,0 +1,7 @@
+package core.library.enums;
+
+public enum Genre {
+	Adventure, Art, Biography, Children, Classic, Comedy, Comic, Computer, Cooking, Crime, Fantasy, Financial,
+	Fiction, History, Horror, Manga, Memoir, Motivational, Music, Mystery, Non_Fiction, Paranormal, Philosophy,
+	Religion, Romance, Science, Science_Fiction, Sports, Sci_Fi, Self_Help, Thriller, Travel,
+}
---
+++ Library/src/core/library/enums/Topic.java
@@ -0,0 +1,6 @@
+package core.library.enums;
+
+public enum Topic {
+	Aviation, National, Celebrity, Crime, E_Sports, Food, Gaming, Health, International, Invention, Job,
+	Lifetyle, Music, Natural_Disaster, Politics, Science, Story, Sport, Tragedy, Trending, Vehicle,
+}
