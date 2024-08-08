33830cb8144dfa1362a547e4a30c5b7646ca92d0
StanleyJo-37 <stanleyjo13w@gmail.com>
2024-08-08 17:51:47 +0700

    Created LiteratureList

---
+++ Library/src/core/personnel/components/LiteratureList.java
@@ -0,0 +1,15 @@
+package core.personnel.components;
+
+import java.util.ArrayList;
+
+import core.library.components.Request;
+//import core.library.literatures.Literature;
+import core.library.literatures.Book;
+//import core.library.literatures.News;
+
+public class LiteratureList {
+
+	private ArrayList<Book> BorrowedBook;
+	private ArrayList<Request> requests;
+	
+}
